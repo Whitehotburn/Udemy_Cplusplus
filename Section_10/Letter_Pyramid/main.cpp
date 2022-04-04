e3dc63190c1938c4b71f9ba05bd8a2d56c5d2772
Whitehotburn <38023623+Whitehotburn@users.noreply.github.com>
2022-04-04 18:43:28 -0400

    Letter_Pyramid

---
+++ Section_10/Letter_Pyramid/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    string input{};
+    cout << "Enter a string to pyramid-ize: ";
+    getline(cin, input);
+
+    size_t total_length = (input.length() * 2) - 1;
+
+    cout << endl; 
+
+    size_t pad_size = input.length() - 1;
+    string pad(pad_size, ' ');
+    
+    for (size_t i{0}; i < input.length(); i ++) {
+        cout << '|' << pad;
+
+        if (i != 0) {
+            size_t tracker{};
+            int direction{1};
+            for (size_t j{0}; j<(i*2)+1; j++) {
+                if (tracker == i) {
+                    direction *= -1;
+                }
+                cout << input[tracker];
+                tracker += direction;
+            }
+        } else
+            cout << input.at(0);
+
+        cout << pad << '|' << endl;
+        pad.pop_back();
+    }
+
+    cout << endl;
+    return 0;
+}
