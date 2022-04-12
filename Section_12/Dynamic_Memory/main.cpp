9e2f1b014409b3180a77c577eb8a77c19586e229
Whitehotburn <38023623+Whitehotburn@users.noreply.github.com>
2022-04-12 19:11:25 -0400

    4/12/22

---
+++ Section_12/Dynamic_Memory/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int *int_ptr {nullptr};
+    int_ptr = new int;
+    cout << int_ptr << endl;
+    delete int_ptr; // Frees up storage
+
+    size_t size{0};
+    double *temp_ptr {nullptr};
+
+    cout << "How many temps? ";
+    cin >> size;
+
+    temp_ptr = new double[size]; // allocate # of doubles in memory
+
+    cout << temp_ptr << endl;
+    delete [] temp_ptr;
+
+    cout << endl;
+    return 0;
+}
