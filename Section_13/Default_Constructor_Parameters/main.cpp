51df3439ce567427b59d2150ae3d0296d3f8c039
Mitchell M <38023623+mmichalak-swe@users.noreply.github.com>
2022-04-15 18:17:15 -0400

    4/15/22

---
+++ Section_13/Default_Constructor_Parameters/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::string;
+using std::endl;
+
+class Player {
+private:
+    string name;
+    int health;
+    int xp;
+
+public:
+    Player(string name_val="None", int health_val=0, int xp_val=0);
+};
+
+    Player::Player(string name_val, int health_val, int xp_val) : name{name_val}, health{0}, xp{0} {
+        cout << "Three-args constructor" << endl;
+    }
+
+int main() {
+    Player empty;
+    Player frank {"Frank"};
+    Player hero {"Hero", 100};
+    Player villain {"Villain", 100, 55};
+
+    return 0;
+}
