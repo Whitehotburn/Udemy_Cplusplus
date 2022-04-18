5204d475780ebca76995c273c08bdf4ff1ca1ee9
Whitehotburn <38023623+Whitehotburn@users.noreply.github.com>
2022-04-18 16:18:03 -0400

    4/18/22

---
+++ Section_14/Mystring_copy_assigment/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "Mystring.h"
+
+int main() {
+    Mystring a {"Hello"};
+    Mystring b;
+    b = a;
+
+    b = "This is a test";
+
+    // Mystring empty;
+    // Mystring larry{"Larry"};
+    // Mystring stooge{larry};
+
+    // empty.display();
+    // larry.display();
+    // stooge.display();
+
+    return 0;
+}
---
+++ Section_14/Mystring_start/Mystring.h
@@ -0,0 +1,19 @@
+#ifndef _MYSTRING_H_
+#define _MYSTRING_H_
+
+class Mystring {
+private:
+    char *str;
+public:
+    Mystring();
+    Mystring(const char *s);
+    Mystring(const Mystring &source);
+    ~Mystring();
+    void display() const;
+    // getters
+    int get_length() const;
+    const char *get_str() const;
+
+};
+
+#endif // _MYSTRING_H_
