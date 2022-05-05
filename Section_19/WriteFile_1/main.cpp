be06dac4bd923990ea12c8c44916a4ac1c75671a
Whitehotburn <38023623+Whitehotburn@users.noreply.github.com>
2022-05-05 19:08:52 -0400

    tested file write

--- Section_19/WriteFile_1/main.cpp
+++ Section_19/WriteFile_1/main.cpp
@@ -5,7 +5,7 @@
 #include <string>
 
 int main()  {
-    std::ofstream out_file {"../output.txt", std::ios::app};
+    std::ofstream out_file {"./output.txt", std::ios::app};
     if (!out_file) {
         std::cerr << "Error creating file" << std::endl;
         return 1;
