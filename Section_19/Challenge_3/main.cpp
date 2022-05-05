b644faf902fe1526f858145e17612c1d62c69ec8
Whitehotburn <38023623+Whitehotburn@users.noreply.github.com>
2022-05-05 18:55:37 -0400

    added find function

--- Section_19/Challenge_3/main.cpp
+++ Section_19/Challenge_3/main.cpp
@@ -6,7 +6,13 @@
 #include <string>
 #include <algorithm>
 
-using namespace std;
+bool find_substring(const std::string &in_word, const std::string &desired_word) {
+    size_t found = in_word.find(desired_word);
+    if (found!=std::string::npos)
+        return true;
+    else
+        return false;
+}
 
 int main() {
     std::ifstream in_file;
@@ -21,10 +27,10 @@ int main() {
     int case_sensitive{};
 
     std::cout << "\nEnter the substring to search for: ";
-    cin >> desired_word;
+    std::cin >> desired_word;
 
     std::cout << "\nCase sensitive? Press 1 for yes, 0 for no: ";
-    cin >> case_sensitive;
+    std::cin >> case_sensitive;
     
     if (!case_sensitive)
         transform(desired_word.begin(), desired_word.end(), desired_word.begin(), ::tolower);
@@ -33,21 +39,21 @@ int main() {
     size_t total_word_count{};
     
     while (in_file >> in_word) {
+        ++total_word_count;
+
         if (!case_sensitive)
             transform(in_word.begin(), in_word.end(), in_word.begin(), ::tolower);
 
-        size_t found = in_word.find(desired_word);
-
-        if (found!=std::string::npos)
+        if (find_substring(in_word, desired_word)) {
             desired_word_count++;
-        total_word_count++;
+        }
     }
 
     std::cout << total_word_count << " words were searched..." << std::endl;
     std::cout << "The substring \"" << desired_word << "\" was found " << desired_word_count << " times." << std::endl;
 
     in_file.close();
-    cout << endl;
+    std::cout << std::endl;
     return 0;
 }
 
