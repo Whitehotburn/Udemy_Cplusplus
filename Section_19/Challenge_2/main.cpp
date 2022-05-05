69d049f228f31eda5d21bddd47599923a1357b40
Whitehotburn <38023623+Whitehotburn@users.noreply.github.com>
2022-05-05 17:37:34 -0400

    updated main & functions

--- Section_19/Challenge_2/main.cpp
+++ Section_19/Challenge_2/main.cpp
@@ -5,6 +5,43 @@
 #include <fstream>
 #include <string>
 
+// Function prototypes
+void print_header(const int &, const int &, const int &);
+void print_footer(const int &, const int &, const int &, const double &average);
+void print_student(const int &, const int &, const int &, const std::string &student, const int &score);
+int process_response(const std::string &response, const std::string &answer_key);
+
+void print_header(const int &total_width, const int &field_1_width, const int &field_2_width) {
+    std::string underline(total_width, '-');
+    std::cout << "\n" << underline << std::endl;
+    std::cout << std::setw(field_1_width) << std::left << "Student"
+              << std::setw(field_2_width + 1) << std::right << "Score" << std::endl;
+    std::cout << underline << std::endl;
+}
+
+void print_footer(const int &total_width, const int &field_1_width, const int &field_2_width, const double &average) {
+    std::string underline(total_width, '-');
+    std::cout << underline << std::endl;
+    std::cout << std::setw(field_1_width) << std::left << "Average score" << "|"
+              << std::setw(field_2_width) << std::right << std::setprecision(1) << std::fixed
+              << average << std::endl;
+    std::cout << underline << "\n" << std::endl;
+}
+
+void print_student(const int &total_width, const int &field_1_width, const int &field_2_width, const std::string &student, const int &score) {
+    std::cout << std::setw(field_1_width) << std::left << student << "|"
+              << std::setw(field_2_width) << std::right << score << std::endl;
+}
+
+int process_response(const std::string &response, const std::string &ans_key) {
+    int score{};
+    for (size_t i = 0; i < ans_key.size(); i++) {
+        if (response.at(i) == ans_key.at(i))
+        score++;
+    }
+    return score;
+}
+
 int main() {
     std::ifstream in_file;
     in_file.open("./responses.txt");
@@ -16,50 +53,30 @@ int main() {
     const int total_width = 35;
     const int field_1_width = 29;
     const int field_2_width = 5;
-    double cum_score{};
-    int num_of_scores{};
+    int cum_score{};
+    int num_of_students{};
+    double average{};
 
-    std::string underline(total_width, '-');
+    print_header(total_width, field_1_width, field_2_width);
 
-    size_t count = 0;
     std::string ans_key{};
-    std::string line{};
-    while (std::getline(in_file, line)) {
-        if (count == 0) {
-            ans_key = line.erase(line.size() - 1);
-            std::cout << "\n" << std::setw(field_1_width) << std::left << "Student"
-                      << std::setw(field_2_width + 1) << std::right << "Score" << std::endl;
-            std::cout << underline << std::endl;
-            count++;
-            continue;
-        }
-        
-        // Odd lines are the student names
-        if (count % 2 != 0) {
-            line.erase(line.size() - 1);
-            std::cout << std::setw(field_1_width) << std::left << line << "|";
-        }
-        // Even lines are the students' scores
-        else if (count % 2 == 0 && count != 0) {
-            size_t score = 0;
-            for (size_t i{0}; i < ans_key.size(); i++) {
-                if (line[i] == ans_key[i])
-                    score += 1;
-            }
-            cum_score += score;
-            num_of_scores += 1;
-            std::cout << std::setw(field_2_width) << std::right << score << std::endl;
-        }
-        count ++;
+    std::string name{};
+    std::string response{};
+
+    in_file >> ans_key;
+
+    while (in_file >> name >> response) {
+        ++num_of_students;
+        int score = process_response(response, ans_key);
+        cum_score += score;
+        print_student(total_width, field_1_width, field_2_width, name, score);
     }
+
+    if (num_of_students != 0)
+        average = static_cast<double>(cum_score)/num_of_students;
+
+    print_footer(total_width, field_1_width, field_2_width, average);
+
     in_file.close();
-
-    std::cout << underline << std::endl;
-    std::cout << std::setw(field_1_width) << std::left << "Average score" << "|"
-              << std::setw(field_2_width) << std::right << std::setprecision(1) << std::fixed
-              << cum_score/num_of_scores << std::endl;
-    std::cout << underline << "\n" << std::endl;
-
-
     return 0;
 }
